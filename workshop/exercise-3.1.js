605bb413179e7de2ed58feb9fa7a6cda3604f0d9
farrell-dan <140118556+farrell-dan@users.noreply.github.com>
2023-10-15 17:27:09 -0400

    Exercise 3.1

--- workshop/exercise-3.1.js
+++ workshop/exercise-3.1.js
@@ -3,7 +3,23 @@
 
 // require the 'request-promise' module.
 
+const request = require("request-promise");
+
 // Returns the current position of the ISS
 const getIssPosition = () => {
   // write some code...
+
+  return request("http://api.open-notify.org/iss-now.json")
+    .then((response) => JSON.parse(response))
+    .then((parsedResponse) => {
+      const lat = parsedResponse.iss_position.latitude;
+      const lng = parsedResponse.iss_position.longitude
+      return {lat, lng }
+    })
+    .catch ((err) => {
+      return err.error ? JSON.parse(err.error) : err;
+    });
 };
+
+getIssPosition().then((result) => console.log(result));
+
