3134846ad99559bc48279400a4bf076ae1174ed9
farrell-dan <140118556+farrell-dan@users.noreply.github.com>
2023-10-15 18:00:06 -0400

    Exercise 3.3

--- workshop/exercise-3.3.js
+++ workshop/exercise-3.3.js
@@ -5,12 +5,26 @@ const opencage = require('opencage-api-client');
 require('dotenv').config();
 
 const getAddressFromPosition = (lat, lng) => {
-  const requestObj = {
-    key: '<MY_API_KEY>',
-    q: '<QUERY_STRING>',
-  };
+  return new Promise((resolve, reject) => {
+      const requestObj = {
+        key: "295c76d1aba24cd7954914b154edf0c9",
+        q: `${lat},${lng}`,
+      };
 
-  // return ...
+       opencage
+      .geocode(requestObj)
+      .then((data) => {
+        if (data && data.results && data.results.length > 0) {
+          const { formatted } = data.results[0];
+          resolve(formatted);
+        } else {
+          reject("Address not found.");
+        }
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
 }
 
 getAddressFromPosition('48.8584', '2.2945').then((response) =>
